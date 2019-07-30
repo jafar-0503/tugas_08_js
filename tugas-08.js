9f488b3f0b67e857d33ed3b974c03035d67a9cf0
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-07-30 23:22:04 +0700

    tugas_08_js
    tugas sudah ok !!!

---
+++ tugas-08.js
@@ -0,0 +1,15 @@
+function object(){
+
+  var produk = {
+      nama_Barang: "Sepatu",
+      size: 40,
+      warna: "Hitam",
+      Merk: "Adidas"
+  }
+
+  produk.size = 39;
+  produk.merk = "Nike";
+  console.log(produk);
+}
+
+  object()
